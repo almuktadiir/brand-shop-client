e8bdde30a2a5df67869f9ef2ae29c3e051bc68c3
almuktadiir <muktadir.j729.com>
2023-10-18 14:53:17 +0600

    brand data read

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
-require('dotenv').config()
-console.log(process.env)
+require('dotenv').config();
+console.log(process.env);
+const brands = require('./brand.json');
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+// brands category
+app.get('/brand', (req, res)=>{
+    res.send(brands);
+})
 
 app.get('/', (req, res)=> {
     res.send('fashion server is running');
