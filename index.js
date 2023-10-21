c56512007c11b117de1e88f82fa58deaadfcdd6d
almuktadiir <muktadir.j729.com>
2023-10-21 13:37:44 +0600

    file resizing

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const brands = require('./brand.json');
 const app = express();
-const port = process.env.PORT || 5000;
+const cors = require('cors');
 require('dotenv').config();
-console.log(process.env);
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port = process.env.PORT || 5000;
+const brands = require('./brand.json');
+// console.log(process.env);
 
 //middleware
 app.use(cors());
@@ -13,7 +13,8 @@ app.use(express.json());
 
 
 
-const uri = "mongodb+srv://addBrand:k5hNUnbS38rg63Wh@cluster0.iawgzs5.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iawgzs5.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,7 +28,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
 
     const database = client.db("brandsDB");
